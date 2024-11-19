19317140862172d31de3c9976bdd4d926213238a
Kamiku <yx3494@nyu.edu>
2024-11-18 22:31:14 -0500

    Update configuration and enhance plotting: reduce stepAvg and stepLimit values, introduce Stats class for improved data handling, and refactor plotMolCoo function to utilize new statistics.

--- plot.py
+++ plot.py
@@ -10,7 +10,21 @@ class Mol:
     def __str__(self):
         return f'{self.id} {self.pos}'
 
-def plotMolCoo(mol: Mol, ts, outfile):
+class Stats:
+    def __init__(self, e1, e2, ke1, ke2, p1, p2, ts, step):
+        self.e1 = e1
+        self.e2 = e2
+        self.ke1 = ke1
+        self.ke2 = ke2
+        self.p1 = p1
+        self.p2 = p2
+        self.ts = ts
+        self.step = step
+        
+    def __str__(self):
+        return f' {self.step} {self.ts} {self.e1} {self.e2} {self.ke1} {self.ke2} {self.p1} {self.p2}'
+
+def plotMolCoo(mol: Mol, ts, outfile, stats: Stats):
     x = []
     y = []
     
@@ -21,18 +35,17 @@ def plotMolCoo(mol: Mol, ts, outfile):
     mark_1 = int(len(mol) / 2 + len(mol) / 8)
     mark_2 = int(len(mol) / 2 + len(mol) / 8 + 1)
     
-    E = "{0:.4f}".format(totEnergy.sum1)
-    Sigma_E = "{0:.4f}".format(totEnergy.sum2)
-    Ek = "{0:.4f}".format(kinEnergy.sum1)
-    Sigma_Ek = "{0:.4f}".format(kinEnergy.sum2)
-    P_1 = "{0:.4f}".format(pressure.sum1)
-    P_2 = "{0:.4f}".format(pressure.sum2)
+    E = "{0:.4f}".format(stats.e1)
+    Sigma_E = "{0:.4f}".format(stats.e2)
+    Ek = "{0:.4f}".format(stats.ke1)
+    Sigma_Ek = "{0:.4f}".format(stats.ke2)
+    P_1 = "{0:.4f}".format(stats.p1)
+    P_2 = "{0:.4f}".format(stats.p2)
     
+    plt.clf()
     plt.plot(x, y, 'o', color='blue')
-    
     plt.plot(x[mark_1], y[mark_1], 'o', color='red')
     plt.plot(x[mark_2], y[mark_2], 'o', color='cyan')
-    
     plt.title('$\Delta t$:' + "{0:.4f}".format(ts) + '; ' +
                 'E:' + E + '; ' +
                 '$\sigma E$:' + Sigma_E + ';\n' +
@@ -46,21 +59,26 @@ def plotMolCoo(mol: Mol, ts, outfile):
 def readOutput(path):
     n = 0
     mols = []
+    stats : Stats = None
     with open(path, 'r') as f:
         lines = f.readlines()
-        step = lines[0].split()[1]
-        ts = lines[1].split()[1]
+        step = int(lines[0].split()[1])
+        ts = float(lines[1].split()[1])
+        E = float(lines[2].split()[1])
+        Ek = float(lines[3].split()[1])
+        P_1 = float(lines[4].split()[1])
+        Sigma_E = float(lines[5].split()[1])
+        Sigma_Ek = float(lines[6].split()[1])
+        P_2 = float(lines[7].split()[1])
+        stats = Stats(E, Sigma_E, Ek, Sigma_Ek, P_1, P_2, ts, step)
         # loop through the lines and extract the data
         n = 0
-        for line in lines[3:]:
+        for line in lines[9:]:
             vals = line.split()
-            mol = Mol(vals[0], vals[1], vals[2])
+            mol = Mol(vals[0], float(vals[1]), float(vals[2]))
             n += 1
             mols.append(mol)
-            
-    return n, mols    
-            
-            
+    return n, mols, stats
             
 if __name__ == '__main__':
     # read the output file
@@ -78,4 +96,6 @@ if __name__ == '__main__':
             for out in outs:
                 # read the output file
                 path = output_dir + mode + '/' + size + '/' + out
-                readOutput(path)
+                n, mols, stats = readOutput(path)
+                # plot the data
+                plotMolCoo(mols, stats.ts, 'output/' + mode + '/' + size + '/' + out.replace('.out', '.png'), stats)
